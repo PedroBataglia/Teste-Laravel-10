237e225ffc39e73bdc017526b278ba8af8980178
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-04-06 16:05:24 -0300

    novos recursos

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Site;
 
-class SiteController
+class SiteController 
 {
     public function contact()
     {
--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,9 @@ use App\Http\Controllers\Admin\{SupportController};
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+Route::get('/forum', [SupportController::class, 'index'])->name('supports.index');
 
-Route::get('/contato', [SiteController::class, 'contact']);
+Route::get('/contato', [siteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
