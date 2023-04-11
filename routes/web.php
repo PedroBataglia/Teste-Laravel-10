67e7b318d8daecb356621b6630ad1336859e870f
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-04-11 11:54:17 -0300

    testando rotas e controllers

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,15 @@ Route::get('/forum', [SupportController::class, 'index'])->name('supports.index'
 
 Route::get('/contato', [siteController::class, 'contact']);
 
+Route::group(['prefix' => 'painel'], function() {
+
+    Route::group(['prefix' => 'recursos_humanos', 'namespace' => 'Admin'], function() {
+
+        Route::get('/comunicados', [ComunicadosController::class, 'index'])->name('comunicados.index');
+    });
+
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
